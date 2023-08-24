e935e73a4c015ca7e7affbf8f13e0b8d5fd0927e
Marek <marek@MacBook-Air-Marek.local>
2023-08-24 17:49:15 +0200

    second

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -48,6 +48,17 @@ export default function Home() {
 
   return (
     <>
+
+          {/* <!-- Google tag (gtag.js) --> */}
+          <Script async src="https://www.googletagmanager.com/gtag/js?id=AW-11239400043"></Script>
+          <Script id="google-tag-manager"> {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'AW-11239400043');`}
+          </Script>
+
          {/* <!-- Google tag (gtag.js) --> */}
         <Script async src="https://www.googletagmanager.com/gtag/js?id=G-VTLH3RL910"></Script>
         <Script id="google-analitycs">{`
--- src/pages/wiedza/szukam-grafika-rozwiazania.tsx
+++ src/pages/wiedza/szukam-grafika-rozwiazania.tsx
@@ -1,8 +1,28 @@
 import React from 'react'
 import Link from 'next/link'
+import Script from 'next/script'
 
 export default function MoreAbout() {
   return (
+    <>
+    {/* <!-- Google tag (gtag.js) --> */}
+    <Script async src="https://www.googletagmanager.com/gtag/js?id=AW-11239400043"></Script>
+          <Script id="google-tag-manager"> {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'AW-11239400043');`}
+          </Script>
+
+         {/* <!-- Google tag (gtag.js) --> */}
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-VTLH3RL910"></Script>
+        <Script id="google-analitycs">{`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-VTLH3RL910');`}
+        </Script>
     <div className='w-screen h-screen bg-[url("/photo.jpeg")] bg-cover '>
       <div className='md:w-[1000px] mx-auto'>
       <div className='text-white w-full px-[10px] mb-[20px] text-[30px] flex justify-start'>
@@ -13,5 +33,6 @@ export default function MoreAbout() {
     <Link href="/"><div className='bg-green-700 px-[15px] py-[10px] pb-[10px] text-[15px] md:text-[22px] my-[20px] w-[300px] rounded text-white mx-auto text-center'>Wróć do formularza</div></Link>
     </div>
   </div>
+  </>
   )
 }
